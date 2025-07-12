be88ec363675983359e0c0c14348ee67cc841e9d
abdout <osmanabdout@hotmail.com>
2025-07-12 22:36:10 +0300

    D

--- src/app/api/settings/route.ts
+++ src/app/api/settings/route.ts
@@ -1,91 +1,91 @@
-import { NextRequest, NextResponse } from "next/server";
-import { connectDB } from "@/lib/connectDB";
-import SettingModel from "@/models/Settings.model";
-import { auth } from "@/lib/auth";
+// import { NextRequest, NextResponse } from "next/server";
+// import { connectDB } from "@/lib/connectDB";
+// import SettingModel from "@/models/Settings.model";
+// import { auth } from "@/lib/auth";
 
-//create and update
-export async function POST(request: NextRequest) {
-  try {
-    const session = await auth();
+// //create and update
+// export async function POST(request: NextRequest) {
+//   try {
+//     const session = await auth();
 
-    if (!session) {
-      return NextResponse.json(
-        {
-          message: "Unauthorized access",
-        },
-        {
-          status: 401,
-        }
-      );
-    }
+//     if (!session) {
+//       return NextResponse.json(
+//         {
+//           message: "Unauthorized access",
+//         },
+//         {
+//           status: 401,
+//         }
+//       );
+//     }
 
-    const { logo, signature } = await request.json();
+//     const { logo, signature } = await request.json();
 
-    await connectDB();
+//     await connectDB();
 
-    const setting = await SettingModel.findOne({ userId: session.user.id });
+//     const setting = await SettingModel.findOne({ userId: session.user.id });
 
-    const payload = {
-      userId: session.user.id,
-      ...(logo && { invoiceLogo: logo }),
-      ...(signature && { signature: signature }),
-    };
+//     const payload = {
+//       userId: session.user.id,
+//       ...(logo && { invoiceLogo: logo }),
+//       ...(signature && { signature: signature }),
+//     };
 
-    //update the document
-    if (setting) {
-      const updateSetting = await SettingModel.findByIdAndUpdate(
-        setting._id,
-        payload
-      );
+//     //update the document
+//     if (setting) {
+//       const updateSetting = await SettingModel.findByIdAndUpdate(
+//         setting._id,
+//         payload
+//       );
 
-      return NextResponse.json({
-        message: "Setting updated successfully",
-      });
-    }
+//       return NextResponse.json({
+//         message: "Setting updated successfully",
+//       });
+//     }
 
-    //create the document
-    const createSetting = await SettingModel.create(payload);
+//     //create the document
+//     const createSetting = await SettingModel.create(payload);
 
-    return NextResponse.json({
-      message: "Setting updated successfully",
-    });
-  } catch (error: any) {
-    return NextResponse.json(
-      {
-        message: error || error?.message || "Something went wrong",
-      },
-      {
-        status: 500,
-      }
-    );
-  }
-}
+//     return NextResponse.json({
+//       message: "Setting updated successfully",
+//     });
+//   } catch (error: any) {
+//     return NextResponse.json(
+//       {
+//         message: error || error?.message || "Something went wrong",
+//       },
+//       {
+//         status: 500,
+//       }
+//     );
+//   }
+// }
 
-//get
-export async function GET(request: NextRequest) {
-  try {
-    const session = await auth();
+// //get
+// export async function GET(request: NextRequest) {
+//   try {
+//     const session = await auth();
 
-    if (!session) {
-      return NextResponse.json(
-        {
-          message: "Unauthorized access",
-        },
-        {
-          status: 401,
-        }
-      );
-    }
+//     if (!session) {
+//       return NextResponse.json(
+//         {
+//           message: "Unauthorized access",
+//         },
+//         {
+//           status: 401,
+//         }
+//       );
+//     }
 
-    const getData = await SettingModel.findOne({ userId : session.user.id })
+//     const getData = await SettingModel.findOne({ userId : session.user.id })
 
-    return NextResponse.json({
-        message : "Success",
-        data : getData
-    })
-  } catch (error : any) {
-    return NextResponse.json({
-        message : error || error?.message || "Something went wrong"
-    })
-  }
-}
+//     return NextResponse.json({
+//         message : "Success",
+//         data : getData
+//     })
+//   } catch (error : any) {
+//     return NextResponse.json({
+//         message : error || error?.message || "Something went wrong"
+//     })
+//   }
+// }
